c33d72de262554ba5efc8c5d5c851d332d0716a1
Axel <axelancerr@gmail.com>
2021-12-12 00:43:16 +0000

    add: day_2_part_1

--- day_2/day_2_part_1.py
+++ day_2/day_2_part_1.py
@@ -1,11 +1,22 @@
 (
     lambda x:
-        print(x[0]["forward"] * (x[0]["down"] - x[0]["up"]))
+        print(x[0]["horizontal"] * x[0]["depth"])
 )(
     (
         lambda y:
-            (y, [y.__setitem__(command[0], y.__getitem__(command[0]) + int(command[1])) for command in list(map(str.split, open("input.txt").readlines()))])
+            (
+                y,
+                [
+                    (
+                        y.__setitem__("horizontal", y["horizontal"] + int(command[1])),
+                        y.__setitem__("depth", y["depth"] + (y["aim"] * int(command[1])))
+                    ) if command[0] == "forward"
+                    else (y.__setitem__("aim", y["aim"] + int(command[1]))) if command[0] == "down"
+                    else (y.__setitem__("aim", y["aim"] - int(command[1]))) if command[0] == "up" else None
+                    for command in list(map(str.split, open("input.txt").readlines()))
+                ]
+            )
     )(
-        {"up": 0, "down": 0, "forward": 0}
+        {"aim": 0, "horizontal": 0, "depth": 0}
     )
 )
