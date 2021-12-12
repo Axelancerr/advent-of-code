3442dbc51b71c98668fc9e4effa1ad07f5ee630f
Axel <axelancerr@gmail.com>
2021-12-12 13:57:05 +0000

    add: day 3 part 1

---
+++ day_3/day_3_part_1.py
@@ -0,0 +1,18 @@
+(
+    lambda x:
+        (
+            (
+                lambda y:
+                    print(int("".join(y[0]), 2) * int("".join(y[1]), 2))
+            )(
+                [
+                    [__import__("collections").Counter([j[i] for j in x]).most_common()[0][0] for i in range(12)],
+                    [__import__("collections").Counter([j[i] for j in x]).most_common()[-1][0] for i in range(12)],
+                ]
+
+            )
+
+        )
+)(
+    list(map(list, map(str.strip, open("input.txt").readlines())))
+)
